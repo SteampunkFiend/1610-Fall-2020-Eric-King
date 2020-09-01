d6c9aade4bcd2d3b516c31e0aa40fb520c2c4c81
Eric King <punisher.ek.ek@gmail.com>
2020-09-01 15:35:12 -0600

    Work on MoverScript and Classes
    Made a MoverScript and learned about classes, variables, and floats.

---
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public float x, y, z;
+    
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+    
+    void Update()
+    {
+        x = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        y = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(x,y,z);
+    }
+}
