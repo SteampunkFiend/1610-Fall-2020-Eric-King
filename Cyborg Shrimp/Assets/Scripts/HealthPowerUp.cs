9565c1802a2c6cd6bc29c8ae00619f1361664e14
Eric King <punisher.ek.ek@gmail.com>
2020-09-16 22:15:51 -0600

    Worked On PowerUps
    Worked on getting some power ups into the scene since I got a little behind. I tried experimenting with increasing health in steps but that didn't work out quite yet. My next goal is to experiment with health decreases in the morning.

---
+++ Cyborg Shrimp/Assets/Scripts/HealthPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    public int Health = 100;
+    
+    //I tried to add something to make health increase in steps but this didn't work to plan.
+    //public int HealthBoost = 15;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        Health++;
+        print(Health);
+    }
+}
