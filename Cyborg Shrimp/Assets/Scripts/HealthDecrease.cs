4240d69a2fafdba7a679cfd7a629ec236c91b2df
Eric King <punisher.ek.ek@gmail.com>
2020-09-17 15:22:11 -0600

    Added Event Scripts
    A lot of this is toying around with collision detection and making an object disappear with events as well as emitting particles.

--- Cyborg Shrimp/Assets/Scripts/HealthDecrease.cs
+++ Cyborg Shrimp/Assets/Scripts/HealthDecrease.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthDecrease : MonoBehaviour
 {
 
    public int Health1 = HealthPowerUp.Health;
+   public UnityEvent healthDecreaseUnityEvent;
 
    //This decrease script works and is based off of the static int in HealthPowerUp but doesn't grab the latest health value.
    public void OnTriggerEnter(Collider other)
    {
       Health1--;
       print(Health1);
+      healthDecreaseUnityEvent.Invoke();
    }
 }
--- Cyborg Shrimp/Assets/Scripts/HealthPowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/HealthPowerUp.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthPowerUp : MonoBehaviour
 {
     public static int Health = 100;
+    public UnityEvent healthUnityEvent;
     
     //I tried to add something to make health increase in steps but this didn't work to plan.
     //public int HealthBoost = 15;
@@ -12,5 +14,6 @@ public class HealthPowerUp : MonoBehaviour
     {
         Health++;
         print(Health);
+        healthUnityEvent.Invoke();
     }
 }
