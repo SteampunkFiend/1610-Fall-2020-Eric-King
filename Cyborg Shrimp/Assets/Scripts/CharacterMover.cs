6ccf870bcec9c21aaa7f19dbe4fc3fb14c6f32fe
Eric King <punisher.ek.ek@gmail.com>
2020-09-22 15:53:42 -0600

    Created a CwC Proto Car and CharacterMover Script
    Made a Create With Code Prototype Car project and imported assets. In Cyborg Shrimp I created a CharacterMover script that can detect collisions/phyiscs, move, and jump.

---
+++ Cyborg Shrimp/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+   public CharacterController controller;
+   public float moveSpeed = 3f, gravity = -9.8f, jumpForce = 30f;
+
+   private Vector3 moveDirection;
+   private float yDirection;
+
+   private void Update()
+   {
+      var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+      moveDirection.Set(moveSpeedInput,yDirection,0);
+
+      yDirection += gravity * Time.deltaTime;
+
+      if (controller.isGrounded  && moveDirection.y < 0)
+      {
+         yDirection = -1f;
+      }
+
+      if (Input.GetButtonDown("Jump"))
+      {
+         yDirection += jumpForce;
+      }
+      
+      controller.Move(moveDirection * Time.deltaTime);
+   }
+}
