6ffd3df9d8d90f20526a8056905271636555dbb5
Eric King <punisher.ek.ek@gmail.com>
2020-09-22 21:05:16 -0600

    Create With Code Car
    Worked on making the controller script and getting the car to move. This stuff is great info but extremely boring to relearn.

---
+++ Create with Code Prototype 1 (Car)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the vehicle forward.
+        print("Move the vehicle forward.");
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
