c0de8927dfc6f3f472ff00cc51123a76d2a43c63
Eric King <punisher.ek.ek@gmail.com>
2020-09-30 23:55:15 -0600

    Working On Create
    Doing some script work for movement.

---
+++ Create with Code Prototype 1 (Car)/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,7,-6);
+    
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Create with Code Prototype 1 (Car)/Assets/Scripts/PlayerController.cs
+++ Create with Code Prototype 1 (Car)/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,17 @@
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
 
-    // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
         //Move the vehicle forward.
         print("Move the vehicle forward.");
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed *horizontalInput);
     }
 }
