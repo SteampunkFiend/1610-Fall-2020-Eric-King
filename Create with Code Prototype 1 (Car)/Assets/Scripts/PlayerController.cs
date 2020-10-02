18b76dea0134f458b77bb38dfd284644456dc6a5
Eric King <punisher.ek.ek@gmail.com>
2020-10-01 23:50:22 -0600

    Create With Code Work
    Finished up a few scripts so I can start on other units tomorrow.

--- Create with Code Prototype 1 (Car)/Assets/Scripts/PlayerController.cs
+++ Create with Code Prototype 1 (Car)/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,20 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 15f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
-        //Move the vehicle forward.
-        print("Move the vehicle forward.");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed *horizontalInput);
+        //Moves the car in the forward and backwards directions based on vertical key input.
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
+        
+        //Rotates/steers the car in the left and right directions based on horizontal key input.
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
