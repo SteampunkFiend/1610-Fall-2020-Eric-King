964c79184dcd181294a714dc5236afcedfce711f
Eric King <punisher.ek.ek@gmail.com>
2020-10-05 15:50:17 -0600

    CwC Work
    Did some work on getting instantiate objects working as well as prefabs and other translation code.

--- Create with Code Prototype 2 (Top Down)/Assets/Scripts/PlayerController.cs
+++ Create with Code Prototype 2 (Top Down)/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,16 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Confirmation for working code.");
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
         //Keep the player in bounds and from falling off the edge.
         if (transform.position.x < -xRange)
         {
